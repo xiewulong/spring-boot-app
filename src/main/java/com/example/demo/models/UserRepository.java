6a1501cb40126245c6da5f45a1904af7d0b054f0
xiewulong <xiewulong@vip.qq.com>
2018-12-20 19:25:28 +0800

    app

--- src/main/java/com/example/demo/models/UserRepository.java
+++ src/main/java/com/example/demo/models/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.models;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
-
-
 
 }
