f2e37eada7767a748942ec7a369f1ba9d18c768c
xiewulong <xiewulong@vip.qq.com>
2018-12-21 08:52:32 +0800

    app

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -15,13 +15,13 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 // import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 // import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+// import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+// import springfox.documentation.service.ApiInfo;
+// import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 // import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
@@ -29,8 +29,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 // import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
 // @EnableCircuitBreaker
 // @EnableConfigServer
